4fa3f12e137058454b5caa4abcf4c54baa2f9c6c
CarlosAlegre <calegreu@gmail.com>
2022-10-07 16:50:52 +0200

    javascrip experimenting

---
+++ TODO.md
@@ -0,0 +1,4 @@
+1.- Use HTML specific tags.
+2.- Finish javascript actions.
+3.- Better looking CSS.
+3.- Responsive design CSS
--- gaimonCustomize.js
+++ gaimonCustomize.js
@@ -4,11 +4,45 @@ async function changeMood() {
     mood_button = await document.getElementById("mood-button")
     if(gaimon_image.getAttribute("src") == "./images/gaimon_stressed.webp") {
         gaimon_image.setAttribute("src", "./images/gaimon_normal.webp")
+        gaimon_image.setAttribute("alt", "Gaimon normal face")
         mood_button.innerHTML = "MAKE HIM ANGRY"    
         console.log("Gaimon mood changed to calmed.")
     } else {
         gaimon_image.setAttribute("src", "./images/gaimon_stressed.webp")
+        gaimon_image.setAttribute("alt", "Gaimon angry face")
         mood_button.innerHTML = "CALM HIM DOWN"    
         console.log("Gaimon mood changed to angry.")
     }
+}
+
+
+async function dressGaimon() {
+    //TODO:  Checked event listener...
+    // const glassesCheckbox = await document.getElementById("glasses-checkbox")
+    // glassesCheckbox.addEventListener()
+
+    // Glasses menu => Element creation
+    const glassesMenu = await document.createElement("select")
+
+    const op1 = await document.createElement("option")
+    op1.innerHTML = "sunglasses"
+    const op2 = await document.createElement("option")
+    op2.innerHTML = "hippie"
+    const op3 = await document.createElement("option")
+    op3.innerHTML = "VR"
+
+    await glassesMenu.appendChild(op1)
+    await glassesMenu.appendChild(op2)
+    await glassesMenu.appendChild(op3)
+    console.log(glassesMenu)
+
+    //TODO: add style
+
+    // TODO: unchecked Event listener...
+    // Remove <select>
+}
+
+
+async function glassesMenu() {
+
 }
